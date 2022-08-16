9ae1bbf93c21afa2c1130a8af7c59f181e5fcce2
lazar-eva <103130591+lazar-eva@users.noreply.github.com>
2022-08-16 21:42:10 +0100

    Add MUST requirements & pikachu cheat code

---
+++ top_trumps.py
@@ -0,0 +1,42 @@
+import random
+import requests
+
+def get_pokemon():
+  pokemon_number = random.randint(1,151)
+
+  url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
+  response = requests.get(url)
+  pokemon = response.json()
+
+  pokemon1 = {
+    'name': pokemon['name'],
+    'id': pokemon['id'],
+    'height': pokemon['height'],
+    'weight': pokemon['weight']
+  }
+  return pokemon1
+
+user_pokemon = get_pokemon()
+opponent_pokemon = get_pokemon()
+
+print('Your Pokemon is {}'.format(user_pokemon['name']))
+stat = input('What stat do you want to compare? (height / weight / id) ')
+print('Your {} is {}'.format(stat, user_pokemon[stat]))
+
+if user_pokemon['name'] == 'pikachu':
+  print("You're lucky today! Your pokemon is Pikachu, you're an ultimate winner!")
+elif opponent_pokemon['name'] == 'pikachu':
+  print("Oh no! Your opponent's pokemon is Pikachu, so he won! Better luck next time!")
+
+def compare_states():
+
+  print("Your opponent's pokemon {} is {}".format(stat, opponent_pokemon[stat]))
+
+  if user_pokemon[stat] > opponent_pokemon[stat]:
+    print('You won this round!')
+  else:
+    print('Your opponent won this round!')
+
+compare_states()
+
+
